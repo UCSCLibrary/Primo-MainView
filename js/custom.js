558a7c83040dfddf5e62a84a9f776b42d2e2268e
Ryan Schwab <rwschwab@ucsc.edu>
2020-08-06 16:06:09 -0700

    Add Request in Melvyl sidebar box. Refactor getQuery() as event listener.

--- js/custom.js
+++ js/custom.js
@@ -30,15 +30,23 @@ var app = angular.module('viewCustom', ['googleTagManager','angularLoad']);
  * Add Haven't Found box
  *
  */
-app.controller('SearchResultListAfterController', ['$scope', function($scope){
+app.controller('SearchResultListAfterController', ['$scope', '$rootScope', function($scope, $rootScope){
   var vm = this;
   vm.getQuery = getQuery;
+
+  // Create a listener for other components trying to get the query.
+  $rootScope.$on("UcscGetQuery", function(){
+    $rootScope.ucscQuery = getQuery();
+  });
 
   // Libchat script
   var libchat = document.createElement("script");
   libchat.src = "https://v2.libanswers.com/load_chat.php?hash=d01223b2d5b712cc1cf9015fef8fa534";
   document.head.appendChild(libchat);
 
+  // TODO: Move to SearchBarAfter
+  // TODO: Refactor to include boolean operators and advanced search where possible
+  // TODO: Refactor to return strings formatted for Melvyl, Hathi, etc
   function getQuery() {
     var queryString = vm.parentCtrl.query;
     if (queryString.includes(",AND;")) {
@@ -222,7 +230,7 @@ app.component('prmSearchBarAfter', {
     template: '<div id="scopeLimitationMessage" class="hidden"><p>Only showing items available online. <a href="/discovery/search?query={{$ctrl.getQuery()}}&tab=Everything&search_scope=MyInst_and_CI&vid=01CDL_SCR_INST:USCS&offset=0">Click to include print and other physical items.</a></p></div>',
 });
 
-app.controller('SearchBarAfterController', ['$scope', function($scope){
+app.controller('SearchBarAfterController', ['$scope', '$rootScope', function($scope, $rootScope){
   var vm = this;
   vm.getQuery = getQuery;
   // Only present this information on the Available Online scope.
@@ -237,7 +245,8 @@ app.controller('SearchBarAfterController', ['$scope', function($scope){
   }, 1000);
 
   function getQuery() {
-    return vm.parentCtrl.$stateParams.query;
+    $rootScope.$emit("UcscGetQuery", {});
+    return $rootScope.ucscQuery;
   }
 }]);
 
@@ -281,6 +290,51 @@ app.controller('LocationItemsAfterController', ['$scope', function($scope){
         }
       }, 100);
     };
+}]);
+
+/*
+ *  Adds a Melyvl "didn't find it?" box on search results
+ */
+app.component('prmPageNavMenuAfter', {
+    bindings: { parentCtrl: '<' },
+    controller: 'PageNavMenuAfterController',
+    template: `
+<md-card id="melvylRequestBox">
+  <md-card-title>
+    <md-card-title-text>
+      <span class="md-headline">Didn't Find It?</span>
+    </md-card-title-text>
+    <button class="zero-margin md-primoExplore-theme md-ink-ripple button-with-icon" type="button" aria-label="Dismiss" ng-click="onClose()">
+      <prm-icon icon-type="svg" svg-icon-set="navigation" icon-definition="ic_close_24px" class="ng-scope ng-isolate-scope">
+        <md-icon md-svg-icon="navigation:ic_close_24px" alt="" class="ng-scope md-primoExplore-theme" aria-hidden="true">
+          <svg width="100%" height="100%" viewBox="0 0 24 24" id="ic_close_24px" y="240" xmlns="http://www.w3.org/2000/svg" fit="" preserveAspectRatio="xMidYMid meet" focusable="false"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path></svg>
+        </md-icon>
+        <prm-icon-after parent-ctrl="$ctrl"></prm-icon-after>
+      </prm-icon>
+    </button>
+  </md-card-title>
+  <md-card-content>
+    <p><a href="https://ucsc.on.worldcat.org/external-search?queryString={{$ctrl.getQuery()}}" target="_blank">Request through Melvyl</a> to borrow books chapters and articles electronically</p>
+  </md-card-content>
+</md-card>`,
+});
+
+app.controller('PageNavMenuAfterController', ['$scope', '$rootScope', function($scope, $rootScope){
+  var vm = this;
+  vm.getQuery = getQuery;
+
+  this.$onInit = function() {
+    $scope.card = angular.element(document.getElementById("melvylRequestBox"));
+  }
+
+  function getQuery() {
+    $rootScope.$emit("UcscGetQuery", {});
+    return $rootScope.ucscQuery;
+  }
+
+  $scope.onClose = function() {
+    $scope.card.addClass('hidden');
+  }
 }]);
 
 /*
