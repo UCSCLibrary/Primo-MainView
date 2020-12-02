96c4d437f468ecbcdae2930d7d5af90c76f75bc4
Ryan Schwab <rwschwab@ucsc.edu>
2020-12-02 13:26:50 -0800

    Trials indicator now appears on brief and full results.

--- js/prmSearchResultJournalIndicationLineAfter.js
+++ js/prmSearchResultJournalIndicationLineAfter.js
@@ -12,7 +12,14 @@ app.controller('prmSearchResultJournalIndicationLineAfter', ['$scope', '$rootSco
   var vm = this;
   if(typeof vm.parentCtrl.item.pnx.display.lds13 !== "undefined") {
     if (vm.parentCtrl.item.pnx.display.lds13.indexOf('*TRIALS*') >= 0) {
-      var recordID = "SEARCH_RESULT_RECORDID_alma" + vm.parentCtrl.item.pnx.display.mms;
+      var briefRecordID = "SEARCH_RESULT_RECORDID_alma" + vm.parentCtrl.item.pnx.display.mms;
+      var fullRecordID = briefRecordID + "_FULL_VIEW";
+      if (document.getElementById(briefRecordID)) {
+        var recordID = briefRecordID;
+      }
+      if (document.getElementById(fullRecordID)) {
+        var recordID = fullRecordID;
+      }
       var trialNode = document.getElementById(recordID).getElementsByClassName("trial-indicator")[0];
       trialNode.innerHTML = '<img src="/discovery/custom/01CDL_SCR_INST-USCS/img/stopwatch.png" width="20" height="20" /><span>Trial Database</span>';
     }
