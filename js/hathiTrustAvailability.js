987a79f38538bee0e275320467366a580207f968
Ryan Schwab <rwschwab@ucsc.edu>
2020-12-02 13:58:51 -0800

    Hathi suppression now uses MMS ID, added new record to suppress.

--- js/hathiTrustAvailability.js
+++ js/hathiTrustAvailability.js
@@ -108,12 +108,12 @@ angular
 
       // New function to suppress mismatched records
       var isMismatched = function() {
-        if (typeof self.prmSearchResultAvailabilityLine.result.pnx.addata.isbn !== 'undefined') {
-          var isbn = self.prmSearchResultAvailabilityLine.result.pnx.addata.isbn[0];
+        if (typeof self.prmSearchResultAvailabilityLine.result.pnx.display.mms[0] !== 'undefined') {
+          var mmsid = self.prmSearchResultAvailabilityLine.result.pnx.display.mms[0];
+          //  SPC scope: A midsummer night's dream video Shakespeare Santa Cruz 1991
+          if (mmsid == '991015985179704876') { return true; }
           // The wisdom of the outlaw : the boyhood deeds of Finn in Gaelic narrative tradition
-          if (isbn == '0520052846') {
-            return true;
-          }
+          if (mmsid == '991009204669704876') { return true; }
         }
         return false;
       }
