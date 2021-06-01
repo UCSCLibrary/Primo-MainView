52b9f56ff28431e5006bc43d5ae42ca1ba907399
Ryan Schwab <rwschwab@ucsc.edu>
2021-06-01 16:19:27 -0700

    Fix undefined var error in alert banner js.

--- js/alertBanner.js
+++ js/alertBanner.js
@@ -6,19 +6,22 @@ let alertBanner = window.setInterval(function(){
     let date = new Date();
 
     /* different date conditions may be added to control when the banner appears */
-    if((date.getFullYear() == 2021 && date.getMonth() < 6)){
+    //if((date.getFullYear() == 2021 && date.getMonth() < 6)){
+    if (false) {
         let prmAlertBar = document.getElementsByClassName('topbar-wrapper');
-        let alertBarDiv = document.createElement('div');
-        alertBarDiv.setAttribute('id', 'customAlertBar');
-        alertBarDiv.setAttribute('style', 'align-content: center;align-items: center;');
-        alertBarDiv.setAttribute('layout-align', 'center center');
-        let alertBarInnerDiv = document.createElement('div');
-        alertBarInnerDiv.setAttribute('style', 'text-align: center;background-color: #fcd02f;padding: 3px 0;font-size: 20px;');
+        if (prmAlertBar) {
+            let alertBarDiv = document.createElement('div');
+            alertBarDiv.setAttribute('id', 'customAlertBar');
+            alertBarDiv.setAttribute('style', 'align-content: center;align-items: center;');
+            alertBarDiv.setAttribute('layout-align', 'center center');
+            let alertBarInnerDiv = document.createElement('div');
+            alertBarInnerDiv.setAttribute('style', 'text-align: center;background-color: #fcd02f;padding: 3px 0;font-size: 20px;');
 
-        /*alert banner message to customize */
-        alertBarInnerDiv.innerHTML = 'Alert: Library Search will be down for maintenance Saturday, May 29 6:00PM – Sunday, May 30 6:00PM';
-        alertBarDiv.appendChild(alertBarInnerDiv);
-        prmAlertBar[0].prepend(alertBarDiv);
+            /*alert banner message to customize */
+            alertBarInnerDiv.innerHTML = 'Alert: Library Search will be down for maintenance Saturday, May 29 6:00PM – Sunday, May 30 6:00PM';
+            alertBarDiv.appendChild(alertBarInnerDiv);
+            prmAlertBar[0].prepend(alertBarDiv);
+        }
         clearInterval(alertBanner);
     }
 }, 5000);
