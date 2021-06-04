353605c05e0fb09cd3bc16c412a3e69bb6cea794
Ryan Schwab <rwschwab@ucsc.edu>
2021-06-04 09:22:28 -0700

    Remove Online Only scope message and link.

--- js/prmSearchBarAfter.js
+++ js/prmSearchBarAfter.js
@@ -11,25 +11,8 @@ app.component('prmSearchBarAfter', {
     templateUrl: 'custom/01CDL_SCR_INST-USCS/html/prmSearchBarAfter.html',
 });
 
-app.controller('SearchBarAfterController', ['$scope', '$rootScope', '$location', '$window', function($scope, $rootScope, $location, $window){
+app.controller('SearchBarAfterController', ['$location', '$window', function($location, $window){
   var vm = this;
-
-  // Only present this information on the Available Online scope.
-  // This changes asynchronously, so we need to check on an interval.
-  // TODO: This could be more elegant with a watch for scope change.
-  var checkScopeInterval = window.setInterval(function(){
-    if (vm.parentCtrl.$stateParams.tab == 'AvailableOnline') {
-      document.getElementById("scopeLimitationMessage").classList.remove('hidden');
-    } else {
-      document.getElementById("scopeLimitationMessage").classList.add('hidden');
-    }
-    // Update the search query if needed
-    if (typeof vm.parentCtrl.$stateParams.query === 'object') {
-      vm.queryString = vm.parentCtrl.$stateParams.query.join('&query=');
-    } else {
-      vm.queryString = vm.parentCtrl.$stateParams.query;
-    }
-  }, 1000);
 
   this.navigateToHomePage = function () {
     var params = $location.search();
