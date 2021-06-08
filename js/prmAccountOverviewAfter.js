439a48740b94bc4113c1dc920346ff84c60c23fa
Ryan Schwab <rwschwab@ucsc.edu>
2021-06-08 10:58:03 -0700

    Change the Account Overview note to link to My ILL

--- js/prmAccountOverviewAfter.js
+++ js/prmAccountOverviewAfter.js
@@ -8,11 +8,7 @@
 app.component('prmAccountOverviewAfter', {
     bindings: { parentCtrl: '<' },
     controller: 'prmAccountOverviewAfterController',
-    template: '<div role="alert" aria-live="assertive" layout-align="center center" class="layout-align-center-center">\
-    <div layout="row" class="bar alert-bar layout-align-center-center layout-row" layout-align="center center">\
-        <span class="bar-text">Change filter from All Institutions to Has Activity to see campuses where you have active loans.</span>\
-    </div>\
-    </div>',
+    templateUrl: 'custom/01CDL_SCR_INST-USCS/html/prmAccountOverviewAfter.html',
 });
 
 app.controller('prmAccountOverviewAfterController', ['$scope', '$rootScope', function($scope, $rootScope){
