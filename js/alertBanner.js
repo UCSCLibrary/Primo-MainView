305aa4be6f61ccd13dd3ad5617ab80d78921fdc2
Ryan Schwab <rwschwab@ucsc.edu>
2021-06-10 12:06:51 -0700

    Schedule Proquest outage banner.

--- js/alertBanner.js
+++ js/alertBanner.js
@@ -5,9 +5,11 @@
 let alertBanner = window.setInterval(function(){
     let date = new Date();
 
-    /* different date conditions may be added to control when the banner appears */
-    //if((date.getFullYear() == 2021 && date.getMonth() < 6)){
-    if (false) {
+    /* Different date conditions may be added to control when the banner appears
+     * Tip: getMonth() returns 0-11.
+     */
+    if((date.getMonth() == 5) && (date.getDate() > 20) && (date.getDate() < 27)){
+    //if (false) {
         let prmAlertBar = document.getElementsByClassName('topbar-wrapper');
         if (prmAlertBar) {
             let alertBarDiv = document.createElement('div');
@@ -18,7 +20,7 @@ let alertBanner = window.setInterval(function(){
             alertBarInnerDiv.setAttribute('style', 'text-align: center;background-color: #fcd02f;padding: 3px 0;font-size: 20px;');
 
             /*alert banner message to customize */
-            alertBarInnerDiv.innerHTML = 'Alert: Library Search will be down for maintenance Saturday, May 29 6:00PM – Sunday, May 30 6:00PM';
+            alertBarInnerDiv.innerHTML = 'Proquest Ebook Central will be unavailable on Saturday, June 26 from 7AM - 3PM; this will impact some of our ebooks. <a href="https://library.ucsc.edu/news/some-ebooks-unavailable-saturday-june-26-from-7am-3pm">More information</a>';
             alertBarDiv.appendChild(alertBarInnerDiv);
             prmAlertBar[0].prepend(alertBarDiv);
         }
