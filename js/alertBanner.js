8b3d605d60913a87c24bcd808068ca74e97ce03c
Ryan Schwab <rwschwab@ucsc.edu>
2021-09-02 09:32:02 -0700

    Alert bar interval now runs only when alerts are enabled.

--- js/alertBanner.js
+++ js/alertBanner.js
@@ -2,28 +2,29 @@
  * Alert banner
  * Code borrowed from UCI
  */
-let alertBanner = window.setInterval(function(){
-    let date = new Date();
 
-    /* Different date conditions may be added to control when the banner appears
-     * Tip: getMonth() returns 0-11.
-     */
-    //if((date.getMonth() <= 7) && (date.getDate() < 27)){
-    if (true) {
-        let prmAlertBar = document.getElementsByClassName('topbar-wrapper');
-        if (prmAlertBar[0]) {
-            let alertBarDiv = document.createElement('div');
-            alertBarDiv.setAttribute('id', 'customAlertBar');
-            alertBarDiv.setAttribute('style', 'align-content: center;align-items: center;');
-            alertBarDiv.setAttribute('layout-align', 'center center');
-            let alertBarInnerDiv = document.createElement('div');
-            alertBarInnerDiv.setAttribute('style', 'text-align: center;background-color: #dff0d8;padding: 3px 0;font-size: 20px;');
+/* Different date conditions may be added to control when the banner appears
+ * Tip: getMonth() returns 0-11.
+ */
+let bannerDate = new Date();
+//if((bannerDate.getMonth() <= 7) && (bannerDate.getDate() < 27)){
+if (true) {
+  // Use an interval to give a 5000ms delay before alert appears
+  let alertBanner = window.setInterval(function(){
+    let prmAlertBar = document.getElementsByClassName('topbar-wrapper');
+    if (prmAlertBar[0]) {
+      let alertBarDiv = document.createElement('div');
+      alertBarDiv.setAttribute('id', 'customAlertBar');
+      alertBarDiv.setAttribute('style', 'align-content: center;align-items: center;');
+      alertBarDiv.setAttribute('layout-align', 'center center');
+      let alertBarInnerDiv = document.createElement('div');
+      alertBarInnerDiv.setAttribute('style', 'text-align: center;background-color: #dff0d8;padding: 3px 0;font-size: 20px;');
 
-            /*alert banner message to customize */
-            alertBarInnerDiv.innerHTML = 'Welcome to UC Library Search!';
-            alertBarDiv.appendChild(alertBarInnerDiv);
-            prmAlertBar[0].prepend(alertBarDiv);
-        }
-        clearInterval(alertBanner);
+      /*alert banner message to customize */
+      alertBarInnerDiv.innerHTML = 'Welcome to UC Library Search!';
+      alertBarDiv.appendChild(alertBarInnerDiv);
+      prmAlertBar[0].prepend(alertBarDiv);
     }
-}, 5000);
+    clearInterval(alertBanner);
+  }, 5000);
+}
