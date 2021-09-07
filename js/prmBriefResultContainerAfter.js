213613c6f01e6a2bed55985cd8375bbe80ceef06
Ryan Schwab <rwschwab@ucsc.edu>
2021-09-07 16:30:37 -0700

    Finding aid feature adapted from Harvard and UCSD.

---
+++ js/prmBriefResultContainerAfter.js
@@ -0,0 +1,41 @@
+/**
+ * Courtesy of https://github.com/cbaksik/HVD2/blob/master/js/prm-brief-result-container-after.js
+ * Adds a "Finding Aid" button
+**/
+app.controller('prmBriefResultContainerAfterCtrl',['$location','$scope',function ($location,$scope) {
+    var vm = this;
+    vm.cssClass = 'finding-aid-brief';
+    vm.findingAid = {'displayLabel':'','linkURL':'','newLinkURL':''};
+    vm.$onInit = () => {
+        // get links data from primo parent-ctrl binding data
+        $scope.$watch('vm.parentCtrl.links',()=>{
+            // find $$Elinktofa
+            if(vm.parentCtrl.links) {
+                for(var i=0; i < vm.parentCtrl.links.length; i++) {
+                    var linkItem=vm.parentCtrl.links[i];
+                    var falink = '';
+                    if(linkItem.displayLabel === 'Thumbnail image') {
+                        vm.findingAid = linkItem;
+                        if(linkItem.linkURL){
+							                falink = linkItem.linkURL;
+                        }
+                        vm.findingAid.newLinkURL = falink;
+                        // add more padding when it is full display page
+                        var param = $location.search();
+                        if(param.docid) {
+                            vm.cssClass = 'finding-aid-full';
+                        }
+                        i = vm.parentCtrl.links.length;
+                    }
+                }
+            }
+        });
+    };
+}]);
+
+app.component('prmBriefResultContainerAfter',{
+    bindings: {parentCtrl:'<'},
+    controller: 'prmBriefResultContainerAfterCtrl',
+    controllerAs: 'vm',
+    templateUrl: 'custom/01CDL_SCR_INST-USCS/html/prmBriefResultContainerAfter.html'
+});
