ee05c6b1f2b6adad9400e71df0a8c90657e0c917
Ryan Schwab <rwschwab@ucsc.edu>
2021-09-22 14:25:52 -0700

    Update S&E banner message.

--- js/alertBanner.js
+++ js/alertBanner.js
@@ -21,7 +21,7 @@ if (true) {
       alertBarInnerDiv.setAttribute('style', 'text-align: center;background-color: #dff0d8;padding: 3px 0;font-size: 20px;');
 
       /*alert banner message to customize */
-      alertBarInnerDiv.innerHTML = 'S&E Library items are inaccessible September 22, 2021 - March 28, 2022. Please request items through UC Library Search. <a href="https://library.ucsc.edu/news/science-engineering-library-physical-collections-not-accessible-during-renovation" target="_blank">See more information</a>.';
+      alertBarInnerDiv.innerHTML = 'S&E Library lower level books are inaccessible September 22, 2021 - March 28, 2022. Please request items through UC Library Search. <a href="https://library.ucsc.edu/news/science-engineering-library-physical-collections-not-accessible-during-renovation" target="_blank">See more information</a>.';
       alertBarDiv.appendChild(alertBarInnerDiv);
       prmAlertBar[0].prepend(alertBarDiv);
     }
