6d6d52a34bcf56abc15ba6e1a3ac87f3bbc754ad
Ryan Schwab <rwschwab@ucsc.edu>
2025-07-02 16:43:38 -0700

    Update browzine integration with new parameters

--- js/browzine.js
+++ js/browzine.js
@@ -2,38 +2,52 @@
  *  Begin BrowZine - Primo Integration...
  */
   window.browzine = {
-    api: "https://public-api.thirdiron.com/public/v1/libraries/89",
+    libraryId: "89",
     apiKey: "19142e7e-026a-4466-8a20-05a3f1735152",
  
     journalCoverImagesEnabled: true,
- 
+    showFormatChoice: true,
+    showLinkResolverLink: true,
+    enableLinkOptimizer: true,
+
     journalBrowZineWebLinkTextEnabled: true,
     journalBrowZineWebLinkText: "View Journal Contents",
- 
+
     articleBrowZineWebLinkTextEnabled: true,
     articleBrowZineWebLinkText: "View Issue Contents",
- 
+
     articlePDFDownloadLinkEnabled: true,
     articlePDFDownloadLinkText: "Download PDF",
- 
+
     articleLinkEnabled: true,
     articleLinkText: "Read Article",
- 
+
     printRecordsIntegrationEnabled: true,
- 
+
     unpaywallEmailAddressKey: "enter-your-email@your-institution-domain.edu",
- 
+
     articlePDFDownloadViaUnpaywallEnabled: true,
     articlePDFDownloadViaUnpaywallText: "Download PDF (via Unpaywall)",
- 
+
     articleLinkViaUnpaywallEnabled: true,
     articleLinkViaUnpaywallText: "Read Article (via Unpaywall)",
- 
+
     articleAcceptedManuscriptPDFViaUnpaywallEnabled: true,
     articleAcceptedManuscriptPDFViaUnpaywallText: "Download PDF (Accepted Manuscript via Unpaywall)",
- 
+
     articleAcceptedManuscriptArticleLinkViaUnpaywallEnabled: true,
     articleAcceptedManuscriptArticleLinkViaUnpaywallText: "Read Article (Accepted Manuscript via Unpaywall)",
+
+    articleRetractionWatchEnabled: true,
+    articleRetractionWatchText: "Retracted Article",
+
+    articleExpressionOfConcernEnabled: true,
+    articleExpressionOfConcernText: "Expression of Concern",
+
+    problematicJournalEnabled: true,
+    problematicJournalText: "Problematic Journal",
+
+    documentDeliveryFulfillmentText: "Request PDF",
   };
  
   browzine.script = document.createElement("script");
